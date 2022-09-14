bd7d3bc1713b5dc31f784e8941a118dc0329c1a4
Elise Kesler <eliseke@des91.fnal.gov>
2022-09-14 17:57:28 -0500

    updated for python 3; finished test pipeline except image proc

--- full_image_proc.py
+++ full_image_proc.py
@@ -17,14 +17,14 @@ import queue
 import csv
 
 #start by asking if its a test run to see if we will run the testing suite or the regular automation
-ask_test = raw_input('Is this a test run? [y/n]: ')
+ask_test = input('Is this a test run? [y/n]: ')
 answer_test = ask_test
 bench_criteria = 0
 test_criteria = 0
 
 #determine if its a benchmark test, in which case some specific exposures will need to be pulled later
 if answer_test == 'y':
-    ask_bench = raw_input('Is this a benchmark test? [y/n]: ')
+    ask_bench = input('Is this a benchmark test? [y/n]: ')
     bench_answer = ask_bench
     if bench_answer == 'y':
         bench_criteria = 1
@@ -36,7 +36,7 @@ if test_criteria == 1:
     filepath = ['../DESGW-Pipeline-Testing-Suite']
     isExist = os.path.exists(filepath[0])
     if isExist:
-        print('gw_workflow found')
+        print('Pipeline testing suite found.')
     else:
         git_command = ['git clone https://github.com/SSantosLab/DESGW-Pipeline-Testing-Suite.git ../DESGW-Pipeline-Testing-Suite' ]
         print('Running '+git_command[0]+'in order to clone necessary folder DESGW-Pipeline-Testing-Suite...')
@@ -54,7 +54,7 @@ if test_criteria == 1:
     error_dag = 0
     process = subprocess.Popen(command[0], bufsize=1, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = process.communicate()
-    print(stdout)
+#     print(stdout)
     f = open('configure_dagmaker_errors.out', 'w')
     f.write(stdout)
     if stderr != None:
@@ -63,7 +63,7 @@ if test_criteria == 1:
     f.close()
     
     if error_dag == 1:
-        continue_question = raw_input('Warning: there was an error with configure_dag.py. Please check the configure_dagmaker_errors.out file for information, then decide if you would like to continue the testing pipeline by answering [y/n]: ')
+        continue_question = input('Warning: there was an error with configure_dag.py. Please check the configure_dagmaker_errors.out file for information, then decide if you would like to continue the testing pipeline by answering [y/n]: ')
         continue_answer = continue_question
         if continue_answer == 'n':
             print('Terminating this program.')
@@ -71,25 +71,62 @@ if test_criteria == 1:
             
         else:
             print('Program continuing.')
-#get the season from the list as numbers
-#    #run_gw_workflow.py --exp_list {season}exposures.list --season {season}
-#     print("Running " + command[0])
-#     error_dag = 0
-#     process = subprocess.Popen(command[0], bufsize=1, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#             stdout, stderr = process.communicate()
-#     f = open('configure_dagmaker_errors.out', 'w')
-#     f.write(stdout)
-#     if stderr != None:
-#           f.write(stderr)
-#             error_dag = 1
-#     f.close()
+            
     
-#     if error_dag == 1:
-#         continue_question = raw_input('Warning: there was an error with configure_dag.py. Please check the configure_dagmaker_errors.out file for information, then decide if you would like to continue the testing pipeline by answering [y/n]: ')
-#         continue_answer = continue_question
-#         if continue_answer == 'n':
-#             print('Terminating this program.')
-#             sys.exit()
+    print('Retrieving season in order to run gw_workflow.py...')
+    filepath = 'dagmaker.rc'
+
+
+    with open(filepath, 'r') as file:
+        data = file.readlines()
+#     print(data[3])
+    line = data[3]
+    if line.endswith('\n'):
+          line_new = line[:-len('\n')]
+    if line_new.startswith('SEASON='):
+          line_fixed = line[len('SEASON='):]
+    season = int(line_fixed)
+#     print(season)
+
+#get the season from the list as numbers
+    
+    gw_command = [f'python3 run_gw_workflow.py --exp_list {season}exposures.list']
+    print("Running " + gw_command[0])
+    error_gw = 0
+    process = subprocess.Popen(gw_command[0], bufsize=1, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = process.communicate()
+    f = open('run_gw_workflow_errors.out', 'w')
+    f.write(stdout)
+    if stderr != None:
+          f.write(stderr)
+          error_gw = 1
+    f.close()
+    
+    if error_gw == 1:
+        continue_question = input('Warning: there was an error with run gw_workflow.py. Please check the run_gw_workflow_errors.out file for information, then decide if you would like to continue the testing pipeline by answering [y/n]: ')
+        continue_answer = continue_question
+        if continue_answer == 'n':
+            print('Terminating this program.')
+            sys.exit()
+            
+    js_command = [f'python3 fetchJobSubStats.py --season {season} --exp_list {season}exposures.list']
+    print("Running " + js_command[0])
+    error_js = 0
+    process = subprocess.Popen(js_command[0], bufsize=1, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = process.communicate()
+    f = open('fetchJobSubStats_errors.out', 'w')
+    f.write(stdout)
+    if stderr != None:
+          f.write(stderr)
+          error_js = 1
+    f.close()
+    
+    if error_js == 1:
+        continue_question = input('Warning: there was an error with fetchJobSubStats.py. Please check the fetchJobSubStats_errors.out file for information, then decide if you would like to continue the testing pipeline by answering [y/n]: ')
+        continue_answer = continue_question
+        if continue_answer == 'n':
+            print('Terminating this program.')
+            sys.exit()
     
             
 #regular pipeline
@@ -111,13 +148,13 @@ elif test_criteria == 0:
     os.chdir('../gw_workflow')
     
     #test season numbers are 2206 and 2208
-    test_season_check = (raw_input("Would you like to use a test season number? [y/n]: "))
+    test_season_check = (input("Would you like to use a test season number? [y/n]: "))
     test_season = test_season_check
     if test_season == ('y'):
     #         #des gw testing suite and season number 
         SEASON = 2208
     #     print('Double checking SEASON is set to 2208. If you need a different season, please restart or manually update dagmaker.rc before you enter your exposures.list file later in the code.')
-        inputted_season = (raw_input("Do you want your season to be 2206 or 2208? Enter [2206] or [2208] only, please: "))
+        inputted_season = (input("Do you want your season to be 2206 or 2208? Enter [2206] or [2208] only, please: "))
         if inputted_season.isdigit():
             SEASON = int(inputted_season)
             print(SEASON)
@@ -154,7 +191,7 @@ elif test_criteria == 0:
         used_seasons.append(integer_value)
 
         #save the event data along with the season number 
-        inputted_season = (raw_input("Enter desired season. If you have no input in mind, enter 'random': "))
+        inputted_season = (input("Enter desired season. If you have no input in mind, enter 'random': "))
         if inputted_season.isdigit():
             SEASON = int(inputted_season)
         elif inputted_season == ('random'):
@@ -173,27 +210,27 @@ elif test_criteria == 0:
                 i = 0
 
             elif int(SEASON) in used_seasons:                         
-                answer = (raw_input("Input matches previously used value. Proceeding with this Season input will overwrite previous files. Would you like to keep this input and overwrite previous files? [y/n]: "))
+                answer = (input("Input matches previously used value. Proceeding with this Season input will overwrite previous files. Would you like to keep this input and overwrite previous files? [y/n]: "))
                 answer_input = answer
 
                 if answer_input == ('y'):
-                    answer_input_2 = (raw_input("Are you SURE you want to overwrite previous values and continue with this number? [y/n]: "))
+                    answer_input_2 = (input("Are you SURE you want to overwrite previous values and continue with this number? [y/n]: "))
                     answer_2 = answer_input_2
                     if answer_2 == ('y'):
-                        new_season = (raw_input("YOU CAn'T OVERWRITE pRevIouS FILeS!!!! Pick a new number."))
+                        new_season = (input("YOU CAn'T OVERWRITE pRevIouS FILeS!!!! Pick a new number."))
                         new_value = int(new_season)
                         SEASON = new_value
                         i = 0   
 
                     else:
-                        new_season = (raw_input("Please enter a new value for SEASON:"))
+                        new_season = (input("Please enter a new value for SEASON:"))
                         new_value = int(new_season)
                         SEASON = new_value
                         i = 0        
                 elif answer_input != ('n'):  
                     raise Exception('You gotta enter y or n or the code will break for now.')
                 else:
-                    new_input = raw_input("Please enter a new value for SEASON:")
+                    new_input = input("Please enter a new value for SEASON:")
                     SEASON = new_input
                     i = 0
 
@@ -234,7 +271,7 @@ elif test_criteria == 0:
 
     def update_parameter(parameter):
 
-        new_parameter_input = (raw_input("What would you like to update to? ")) 
+        new_parameter_input = (input("What would you like to update to? ")) 
         new_parameter = str(new_parameter_input)
         return new_parameter
 
@@ -243,10 +280,10 @@ elif test_criteria == 0:
     def ask_restart():
         j = 0
         while j == 0:
-            restart_or_no = (raw_input('Would you like to continue to update parameters? [y/n/help] '))
+            restart_or_no = (input('Would you like to continue to update parameters? [y/n/help] '))
             answer_restart = restart_or_no
             if restart_or_no == ('y'):
-                update_more = (raw_input("If you know what parameter you'd like to update, type it here. For syntax/a list of possible updates, type 'help': "))
+                update_more = (input("If you know what parameter you'd like to update, type it here. For syntax/a list of possible updates, type 'help': "))
                 update_value = update_more
                 i = 0
                 j = 1
@@ -265,7 +302,7 @@ elif test_criteria == 0:
         return {'i': i, 'update_value': update_value}
 
 
-    update_other_stuff = (raw_input("If you know a dagmaker.rc parameter you'd like to update besides season, type it here. Enter 'n' for no if you are done updating dagmaker. For syntax/a list of possible updates, type 'help'." ))
+    update_other_stuff = (input("If you know a dagmaker.rc parameter you'd like to update besides season, type it here. Enter 'n' for no if you are done updating dagmaker. For syntax/a list of possible updates, type 'help'." ))
     update = update_other_stuff 
 
     parameters_strings = ["RM_MYTEMP, JOBSUB_OPTS, JOBSUB_OPTS_SE, RESOURCES, IGNORECALIB, DESTCACHE, TEFF_CUT_g, TEFF_CUT_i, TEFF_CUT_r, TEFF_CUT_Y, TEFF_CUT_z, TEFF_CUT_u, TWINDOW, MIN_NITE, MAX_NITE, SKIP_INCOMPLETE_SE, DO_HEADER_CHECK, WRITEDB"]
@@ -288,7 +325,7 @@ elif test_criteria == 0:
             i = new_values_rm['i']      
 
         elif (update == 'JOBSUB_OPTS'):
-            ask_sure = raw_input('JOBSUB_OPTS update notes: The default for this value likely does not need to be changed. Are you sure you want to proceed? [y/n]: ')
+            ask_sure = input('JOBSUB_OPTS update notes: The default for this value likely does not need to be changed. Are you sure you want to proceed? [y/n]: ')
             answer_sure = ask_sure
             if answer_sure == 'y':
                 JOBSUB_OPTS = update_parameter(JOBSUB_OPTS)
@@ -302,7 +339,7 @@ elif test_criteria == 0:
                 i = new_values_jo['i']
 
         elif (update == 'JOBSUB_OPTS_SE'):
-            ask_sure = raw_input('JOBSUB_OPTS_SE update notes: The default for this value likely does not need to be changed. Are you sure you want to proceed? [y/n]: ')
+            ask_sure = input('JOBSUB_OPTS_SE update notes: The default for this value likely does not need to be changed. Are you sure you want to proceed? [y/n]: ')
             answer_sure = ask_sure
             if answer_sure == 'y':
                 JOBSUB_OPTS_SE = update_parameter(JOBSUB_OPTS_SE)
@@ -316,7 +353,7 @@ elif test_criteria == 0:
                 i = new_values_jos['i']
 
         elif (update == 'RESOURCES'):
-            ask_sure = raw_input('RESOURCES update notes: The default for this value likely does not need to be changed. Are you sure you want to proceed? [y/n]: ')
+            ask_sure = input('RESOURCES update notes: The default for this value likely does not need to be changed. Are you sure you want to proceed? [y/n]: ')
             answer_sure = ask_sure
             if answer_sure == 'y':
                 RESOURCES = update_parameter(RESOURCES)
@@ -386,7 +423,7 @@ elif test_criteria == 0:
 
         elif (update == 'MIN_NITE'):
             print('ONLY use this option if you have nothing but late-time templates. It should be commented out for standard nightly diffim running.')
-            ask_sure = raw_input('Are you sure you want to proceed with updating MIN_NITE? [y/n]: ')
+            ask_sure = input('Are you sure you want to proceed with updating MIN_NITE? [y/n]: ')
             answer_sure = ask_sure
             if answer_sure == 'y':
                 MIN_NITE = update_parameter(MIN_NITE)
@@ -401,7 +438,7 @@ elif test_criteria == 0:
 
         elif (update == 'MAX_NITE'):
             print('Use only if you want to avoid using images taken after MAX_NITE as templates.')
-            ask_sure = raw_input('Are you sure you want to proceed with updating MIN_NITE? [y/n]: ')
+            ask_sure = input('Are you sure you want to proceed with updating MIN_NITE? [y/n]: ')
             answer_sure = ask_sure
             if answer_sure == 'y':
                 MAX_NITE = update_parameter(MAX_NITE)
@@ -423,7 +460,7 @@ elif test_criteria == 0:
 
         elif (update == 'DO_HEADER_CHECK'):
             print('DO_HEADER_CHECK info: Turn off header check for FIELD, OBJECT, TILING if you want save time. Can do that if you have already fixed the headers elsewhere (for example when copying from DESDM).')
-            ask_sure = raw_input('Are you sure you want to proceed with updating DO_HEADER_CHECK? ')
+            ask_sure = input('Are you sure you want to proceed with updating DO_HEADER_CHECK? ')
             answer_sure = ask_sure
             if answer_sure == 'y':
                 print('Syntax is [0/1].')
@@ -735,26 +772,26 @@ elif test_criteria == 0:
 
     if bench_criteria:
         print("We haven't made the benchmark test exp list yet. This is a placeholder.")
-        inputted_exp_list = (raw_input("Please input the filepath to your exp.list file, relative to gw_workflow or as a full path: "))
+        inputted_exp_list = (input("Please input the filepath to your exp.list file, relative to gw_workflow or as a full path: "))
 
         isExist = os.path.exists(inputted_exp_list)
 
     #         subprocess.check_output(new_command[0], stderr=subprocess.STDOUT)
 
         if not isExist:
-            inputted_exp_list = (raw_input("Error, could not find your .list file. Please check location then input the filepath relative to gw_workflow one more time: "))
+            inputted_exp_list = (input("Error, could not find your .list file. Please check location then input the filepath relative to gw_workflow one more time: "))
 
 
     else:
 
-        inputted_exp_list = (raw_input("Please input the filepath to your exp.list file, relative to gw_workflow or as a full path: "))
+        inputted_exp_list = (input("Please input the filepath to your exp.list file, relative to gw_workflow or as a full path: "))
 
         isExist = os.path.exists(inputted_exp_list)
 
     #         subprocess.check_output(new_command[0], stderr=subprocess.STDOUT)
 
         if not isExist:
-            inputted_exp_list = (raw_input("Error, could not find your .list file. Please check location then input the filepath relative to gw_workflow one more time: "))
+            inputted_exp_list = (input("Error, could not find your .list file. Please check location then input the filepath relative to gw_workflow one more time: "))
 
     # filepath = 'exposures_jul27.list'
     filepath = inputted_exp_list
