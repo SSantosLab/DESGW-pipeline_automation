5bdbcecd0f228c3ec108c38f7fa7cd7e6527cc13
Elise Kesler <eliseke@des91.fnal.gov>
2022-11-01 10:42:32 -0500

    shell script temporary path updates

--- full_pipeline_automation.sh
+++ full_pipeline_automation.sh
@@ -1,8 +1,16 @@
-conda activate des18a
-source ../gw_workflow/setup_img_proc.sh
+#conda activate des18a
+#source ../gw_workflow/setup_img_proc.sh
 
-. /cvmfs/des.opensciencegrid.org/eeups/startupcachejob31i.sh
-conda deactivate
+export CONDA_DIR=/cvmfs/des.opensciencegrid.org/fnal/anaconda2
+source $CONDA_DIR/etc/profile.d/conda.sh
+
+#. /cvmfs/des.opensciencegrid.org/eeups/startupcachejob31i.sh
+
+. /data/des80.a/data/desgw/BBH_Global/200224/gw_workflow_0310/debugging/startupcachejob31i_nfs_2.sh
+export JOBSUB_PYVER=python2.7-ucs4
+JOBSUB_PYVER=python2.7-ucs4
+
+#conda deactivate
 conda activate des20a
 
 python2 full_image_proc_python2.py
